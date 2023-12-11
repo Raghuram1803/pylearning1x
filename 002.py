fb2924f7dd966c21dd193f0db2029946ee4f80d4
Raghuram1803 <51287065+Raghuram1803@users.noreply.github.com>
2023-12-11 08:39:10 +0530

    Add files via upload

---
+++ 002.py
@@ -0,0 +1,14 @@
+#print name of user by taking from input command
+name = input("Enter your name")
+print(name)
+#take first name, last name of user and print with sep and with /t
+fname = input("enter your first name")
+print(fname)
+Sname = input("enter your last name")
+print(Sname)
+print(fname,Sname,sep="_",end="\t")
+#take user name as input and say with message: you are welcome "name" to the python class
+name = input("Enter your name")
+print(name)
+print(name,"you are welcome to the python class")
+print("you are welcome",name,"to the python class")
