9c40e0bb97f204a25497ae4f4fde53eb1c4f5841
Raghuram1803 <51287065+Raghuram1803@users.noreply.github.com>
2024-02-17 10:57:35 +0530

    Add files via upload

---
+++ break loop.py
@@ -0,0 +1,13 @@
+#break and ccontinue with the loops EX: From 1-10 print till 5
+
+#count = 0
+#while count<=10:
+ ##  if count>= 5:
+   #     break
+   # count= count+1
+
+#while
+i= 1 #from where you want to print
+while i<=5: #condition
+ print ("Raghuram",i ) # what you want to print
+ i = i+1 #increment
